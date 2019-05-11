9ba495d62a0a431d55d3c344083d8337178d4e8b
rotimi-babalola <rotimi.babalola@andela.com>
2019-05-11 14:22:13 +0100

    add sortby name functionality

---
+++ client/src/components/Controls.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import '../styles/controls.scss';
+
+const Controls = ({ onChange }) => (
+  <div className="controls-container">
+    <div className="select-container">
+      <p>Sort by: </p>
+      <select className="form-control select" onChange={onChange}>
+        <option value="default">Default</option>
+        <option value="name">Name</option>
+      </select>
+    </div>
+    <input type="text" className="form-control" placeholder="Search by name" />
+  </div>
+);
+
+Controls.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Controls;
--- client/src/components/PokemonSpecies.jsx
+++ client/src/components/PokemonSpecies.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { uniqueId } from 'lodash';
+
 import pokeApiWrapper from '../api';
+
 import PokemonSpecie from './PokemonSpecie';
+import Controls from './Controls';
 import Loading from './Loading';
 
 import '../styles/pokemon-species.scss';
@@ -16,6 +19,8 @@ class PokemonSpecies extends React.Component {
       pokemonSpecies: [],
       error: false,
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   async componentDidMount() {
@@ -31,6 +36,39 @@ class PokemonSpecies extends React.Component {
     }
   }
 
+  async handleChange(evt) {
+    const sortByField = evt.target.value;
+
+    const { pokemonSpecies } = this.state;
+
+    if (sortByField !== 'default') {
+      const sortedSpecies = pokemonSpecies.slice().sort((a, b) => {
+        if (a[sortByField] < b[sortByField]) {
+          return -1;
+        }
+        if (a[sortByField] > b[sortByField]) {
+          return 1;
+        }
+        return 0;
+      });
+
+      this.setState({
+        pokemonSpecies: sortedSpecies,
+      });
+    } else {
+      try {
+        const response = await pokeApiWrapper.getAllPokemonSpecies(LIMIT);
+        this.setState({
+          pokemonSpecies: response.data.results,
+        });
+      } catch (error) {
+        this.setState({
+          error: true,
+        });
+      }
+    }
+  }
+
   render() {
     if (!this.state.pokemonSpecies.length) {
       return <Loading />;
@@ -43,7 +81,7 @@ class PokemonSpecies extends React.Component {
     return (
       <React.Fragment>
         <h1 className="heading">Pokemon Species</h1>
-        <div className="controls-container">kfkf</div>
+        <Controls onChange={this.handleChange} />
         <div className="card-wrapper">
           {this.state.pokemonSpecies.map(el => (
             <PokemonSpecie name={el.name} url={el.url} key={uniqueId()} />
